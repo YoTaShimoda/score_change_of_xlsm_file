242678f32ae6519fbc90b22e190d3396406c8f9a
下田陽太 <shimoda@weseek.co.jp>
2019-11-18 01:02:36 +0900

    2回目置換終了

--- get_db_data.py
+++ get_db_data.py
@@ -4,21 +4,26 @@ import score
 client = MongoClient('localhost', 27017)
 db = client.score_database
 collection = db.test_collection
-file_name = '標準得点換算表.xlsx'
-sheet_name = '2018年OSIデータ'
+file_name = 'sakaguti.xlsx'
+sheet_name = '2019年OSIデータ'
 file = openpyxl.load_workbook(file_name)
 sheet = file[sheet_name]
 
 colm = 3
 for i in range(0,14):
     number = score.number[i]
-    for a in range(2,57):
+    for a in range(2,63):
         value = sheet.cell(row=a, column=colm).value
+        print(value)
         score_value = collection.find_one({'$and': [{'number':number},{'origin':value}]})
-        print(score_value['score'])
-        sheet.cell(row=a, column = colm).value = score_value['score']
+        try:
+            print(score_value)
+            sheet.cell(row=a, column = colm).value = score_value['score']
+        except:
+            print('continue')
+            continue
     colm += 1
 
-file.save(file_name)
+file.save('sakaguti_answer.xlsx')
 
 
